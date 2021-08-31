ccfbec03e2fbe969115a2fd0bb1a4d5cf1da71b3
KevinWh0 <45321184+KevinWh0@users.noreply.github.com>
2021-08-30 20:45:28 -0700

    added right panel

--- scripts/Canvas/canvasSizer.js
+++ scripts/Canvas/canvasSizer.js
@@ -4,16 +4,15 @@ import { height, width } from "../toolbox.js";
 export function reCalculateSize(tabWidth, tabHeight) {
   let canvas = document.getElementById("canvas");
   let leftPanelCanvas = document.getElementById("ObjectTreeCanvas");
+  let rightPanelCanvas = document.getElementById("rightPanel");
 
-  let leftPanelW =
-    leftPanelCanvas.style.width != ""
-      ? leftPanelCanvas.style.width
-      : leftPanelCanvas.offsetWidth;
+  let leftPanelW = leftPanelCanvas.offsetWidth;
+
+  let rightPanelW = rightPanelCanvas.offsetWidth;
 
   canvas.style.left = leftPanelW + "px";
   canvas.style.top = 40 + "px";
-
-  canvas.width = tabWidth - leftPanelW;
+  canvas.width = tabWidth - leftPanelW - rightPanelW;
   canvas.height = tabHeight - 40;
 }
 
@@ -26,6 +25,17 @@ export function initCanvasSizer() {
 
   $(function () {
     $("#leftPanel").resizable({
+      handles: "e",
+      resize: function (e, ui) {
+        reCalculateSize(width, height);
+      },
+    });
+  });
+
+  $(function () {
+    $("#rightPanel").resizable({
+      handles: "w",
+
       resize: function (e, ui) {
         reCalculateSize(width, height);
       },
