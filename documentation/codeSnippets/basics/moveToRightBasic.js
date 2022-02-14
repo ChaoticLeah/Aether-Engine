37db7d2d4ffffcba7fd207dd2f8d52da394af57b
KevinWh0 <45321184+KevinWh0@users.noreply.github.com>
2022-02-13 22:00:15 -0800

    Doc changes + dep file update

--- documentation/codeSnippets/basics/moveToRightBasic.js
+++ documentation/codeSnippets/basics/moveToRightBasic.js
@@ -1,6 +1,6 @@
 function onStart(parent) {}
 
 function update(parent) {
-  //Each frame we will move the object to the right 1 pixel
-  parent.x++;
+  //Each frame we will move the object to the right 1 unit
+  parent.incrementX(1);
 }
