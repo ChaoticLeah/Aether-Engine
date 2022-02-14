69bcedeb90d8729e245704f5217947b21da5e91e
KevinWh0 <45321184+KevinWh0@users.noreply.github.com>
2022-02-13 20:42:58 -0800

    bug fix

--- scripts/Objects/Object.js
+++ scripts/Objects/Object.js
@@ -8,6 +8,10 @@ export let inEditor = true;
 export let globalOffsetX = 0;
 export let globalOffsetY = 0;
 
+function calcMovementPer60Frames(speed) {
+  return (60 / (fps | 60)) * speed;
+}
+
 export class GameObject {
   components = [];
   constructor(properties) {
@@ -131,6 +135,28 @@ export class GameObject {
 
   getProperties() {
     return this.components[0].properties;
+  }
+
+  //incrementers
+  incrementX(x) {
+    this.setX(
+      Number(this.components[0].properties.x) + calcMovementPer60Frames(x)
+    );
+  }
+  incrementY(y) {
+    this.setY(
+      Number(this.components[0].properties.y) + calcMovementPer60Frames(y)
+    );
+  }
+  incrementW(w) {
+    this.setW(
+      Number(this.components[0].properties.w) + calcMovementPer60Frames(w)
+    );
+  }
+  incrementH(h) {
+    this.setH(
+      Number(this.components[0].properties.h) + calcMovementPer60Frames(h)
+    );
   }
 }
 
