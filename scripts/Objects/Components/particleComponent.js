ee9131a1a3fad583cde62903cc4888f31f3e2919
KevinWh0 <45321184+KevinWh0@users.noreply.github.com>
2023-12-26 14:59:46 +0100

    particles are a LOT better
    also started work on exporting vars from scripts

--- scripts/Objects/Components/particleComponent.js
+++ scripts/Objects/Components/particleComponent.js
@@ -4,12 +4,13 @@ import { File } from "../../AssetPanel/file.js";
 import { getFile } from "../../AssetPanel/fileManager.js";
 import { addInfoPopup, popupTypes } from "../../Popups/popupManager.js";
 import { fill, rect, renderImage } from "../../toolbox.js";
+import { globalOffsetX, globalOffsetY } from "../Object.js";
 
 export class ParticleComponent extends Component {
   componentName = Component.ParticleComponent || "Particle Component";
 
   childrenParticles = [];
-
+  tick = 0
   //image = undefined;
   constructor(parentObject, image) {
     if (typeof image != "object") image = { image: image };
@@ -19,20 +20,25 @@ export class ParticleComponent extends Component {
       image: propertyTypes.FILE_INPUT,
       width: propertyTypes.NUMBER_INPUT,
       height: propertyTypes.NUMBER_INPUT,
-      velocityX: propertyTypes.NUMBER_INPUT,
-      velocityY: propertyTypes.NUMBER_INPUT,
+      minVelocityX: propertyTypes.NUMBER_INPUT,
+      maxVelocityX: propertyTypes.NUMBER_INPUT,
+      minVelocityY: propertyTypes.NUMBER_INPUT,
+      maxVelocityY: propertyTypes.NUMBER_INPUT,
+      drag: propertyTypes.NUMBER_INPUT,
+      spawnRate: propertyTypes.NUMBER_INPUT,
       rotates: propertyTypes.BOOLEAN_INPUT,
       lifetime: propertyTypes.NUMBER_INPUT,
     };
+    
   }
 
   init(
     parentObject,
     image = "",
-    width = 0,
-    height = 0,
-    velocityX = 0,
-    velocityY = 0,
+    width = 20,
+    height = 20,
+    velocityX = 10,
+    velocityY = 10,
     rotates = false
   ) {
     this.parentObject = parentObject;
@@ -40,10 +46,14 @@ export class ParticleComponent extends Component {
       image: image,
       width: width,
       height: height,
-      velocityX: velocityX,
-      velocityY: velocityY,
+      minVelocityX: velocityX,
+      maxVelocityX: velocityX,
+      minVelocityY: velocityY,
+      maxVelocityY: velocityY,
+      drag: 1,
+      spawnRate: 1,
       rotates: rotates,
-      lifetime: 0,
+      lifetime: 100,
     };
   }
 
@@ -65,21 +75,27 @@ export class ParticleComponent extends Component {
 
   //This one is ran in the editor
   display() {
+    this.tick++
     if (getFile(this.properties.image) != undefined) {
       //loop through all the particles
       for (let i = 0; i < this.childrenParticles.length; i++) {
         let particle = this.childrenParticles[i];
+        
+        if(parseFloat(this.properties.drag) > 0){
+          this.childrenParticles[i].velocityX /= parseFloat(this.properties.drag) + 1
+          this.childrenParticles[i].velocityY /= parseFloat(this.properties.drag) + 1
+        }
         //draw the image
         renderImage(
           getFile(particle.image).data,
-          particle.x,
-          particle.y,
+          particle.x + globalOffsetX,
+          particle.y + globalOffsetY,
           particle.width,
           particle.height,
-          particle.rotates,
-          particle.velocityX,
-          particle.velocityY,
-          particle.lifetime
+          // particle.rotates,
+          // particle.velocityX,
+          // particle.velocityY,
+          // particle.lifetime
         );
         //update the position
         particle.x += particle.velocityX;
@@ -93,7 +109,8 @@ export class ParticleComponent extends Component {
           this.childrenParticles.splice(i, 1);
         }
       }
-      this.addParticle();
+      if(this.tick % parseFloat(this.properties.spawnRate) == 0)
+        this.addParticle();
       // renderImage(
       //   getFile(this.properties.image).data,
       //   this.parentObject.getX(),
@@ -107,16 +124,22 @@ export class ParticleComponent extends Component {
   //This one isnt ran in the editor
   update() {}
 
+  getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
   addParticle() {
     //add it as a json object
     let particle = {
       image: this.properties.image,
-      x: this.parentObject.getGlobalOffsetX(),
-      y: this.parentObject.getGlobalOffsetY(),
+      x: this.parentObject.getGlobalOffsetWithoutCameraX(),
+      y: this.parentObject.getGlobalOffsetWithoutCameraY(),
       width: this.properties.width,
       height: this.properties.height,
-      velocityX: this.properties.velocityX * Math.random(),
-      velocityY: this.properties.velocityY * Math.random(),
+      velocityX: parseFloat(this.properties.minVelocityX) + this.getRandomInt(parseFloat(this.properties.maxVelocityX) - parseFloat(this.properties.minVelocityX)),
+      velocityY: parseFloat(this.properties.minVelocityY) + this.getRandomInt(parseFloat(this.properties.maxVelocityY) - parseFloat(this.properties.minVelocityY)),
+      // velocityX: 10,
+      // velocityY: 10,
       rotates: this.properties.rotates,
       lifetime: this.properties.lifetime,
     };
--- scripts/Objects/Components/scriptComponent.js
+++ scripts/Objects/Components/scriptComponent.js
@@ -19,6 +19,48 @@ export class ScriptComponent extends Component {
   init(parentObject, script = "") {
     this.parentObject = parentObject;
     this.properties = { script: script };
+    this.parseScriptForExports();
+  }
+
+  getType(value) {
+    if (!isNaN(value)) {
+      return propertyTypes.NUMBER_INPUT;
+    } else if (value === "true" || value === "false") {
+      return propertyTypes.TOGGLE_INPUT;
+    } else if (/^['"](.+)['"]$/.test(value)) {
+      return propertyTypes.TEXT_INPUT;
+    } else {
+      return undefined;
+    }
+  }
+
+  parseScriptForExports() {
+    const scriptResource = getFile(this.properties.script)
+    if(scriptResource == undefined) return
+    let fileData = scriptResource.rawData;
+
+    // Regular expression to match exported variable declarations
+    const exportRegex = /\bexport\s+let\s+(\w+)\s*=\s*(.*?);/g; ///\bexport\s+let\s+(\w+)\s*=/g;
+
+    // Array to store matched variable names
+    let exportedVars = [];
+
+    // Match the regular expression in the fileData
+    let match;
+    while ((match = exportRegex.exec(fileData)) !== null) {
+      // The first capturing group (match[1]) contains the variable name
+
+      this.objectPropertyTypes[match[1]] = this.getType(match[2])
+      this.properties[match[1]] = match[2]
+
+      exportedVars.push({
+        varName: match[1],
+        value: match[2],
+      });
+    }
+
+    // Log or use the exported variable names as needed
+    console.log("Exported variables:", exportedVars);
   }
 
   initValues() {
@@ -35,5 +77,6 @@ export class ScriptComponent extends Component {
 
       return;
     }
+    this.parseScriptForExports();
   }
 }
